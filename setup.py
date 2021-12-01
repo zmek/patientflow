768f86e0170904fe77cb0692353b21d77e8483d3
Tom Monks <thomas.m.monks@gmail.com>
2021-12-01 15:05:04 +0000

    SETUP: updated comments

--- setup.py
+++ setup.py
@@ -7,28 +7,30 @@ with open("requirements.txt") as f:
     for library in f.read().splitlines():
         requirements.append(library)
 
+# this is documentation...
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
+    # the _2222 postfix is something I can change when using testpypi
     name="pypi-template_2222",
-    #there must be an way to auto tick up the version number...
+    # set version number using value read in from __init__()
     version=__version__,
     author="Thomas Monks",
-    #I've created a specific email account before and forwarded to my own.
+    # I tend to create a package email account before and forwarded to my own.
     author_email="generic@genericemail.com",
     license="The MIT License (MIT)",
     description="A short, but useful description to appear on pypi",
-    #read in from readme.md and will appear on PyPi
+    # read in from readme.md and will appear on PyPi
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TomMonks/pypi-template",
+    url="https://github.com/health-data-science-OR/pypi-template",
     packages=setuptools.find_packages(),
-    #if true look in MANIFEST.in for data files to include
+    # if true look in MANIFEST.in for data files to include
     include_package_data=True,
-    #2nd approach to include data is include_package_data=False
+    # 2nd approach to include data is include_package_data=False
     package_data={"test_package": ["data/*.csv"]},
-    #these are for documentation 
+    # these are for PyPi documentation 
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -37,5 +39,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6.9',
+    # pip requirements 
     install_requires=requirements,
 )
