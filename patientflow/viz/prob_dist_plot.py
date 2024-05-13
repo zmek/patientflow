8ae10157c6afc59ec42b31bf831b3d9700b5772f
zmek <zella.king@gmail.com>
2024-05-13 18:47:04 +0100

    add default colour to prob dist plot

--- patientflow/viz/prob_dist_plot.py
+++ patientflow/viz/prob_dist_plot.py
@@ -1,11 +1,12 @@
 from matplotlib import pyplot as plt
 
-def prob_dist_plot(prob_dist_data, title_, directory_path = None, figsize = (6,3), include_titles = False, truncate_at_beds=20, text_size = None):
+def prob_dist_plot(prob_dist_data, title_, directory_path = None, figsize = (6,3), include_titles = False, truncate_at_beds=20, text_size = None, bar_colour='#5B9BD5'):
             
     plt.figure(figsize=figsize)
     file_name = title_.replace(' ', '_').replace('/n', '_').replace('%', 'percent') + '.png'
     plt.bar(prob_dist_data.index[0:truncate_at_beds+1], 
-            prob_dist_data['agg_proba'].values[0:truncate_at_beds+1])
+            prob_dist_data['agg_proba'].values[0:truncate_at_beds+1],
+            color=bar_colour)
     
     if text_size:
 
