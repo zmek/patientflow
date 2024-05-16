e51966aa4d2ff12e66d5bc286626d233a76347dc
zmek <zella.king@gmail.com>
2024-05-16 17:50:34 +0100

    format x axis in prob dist plot

--- patientflow/viz/prob_dist_plot.py
+++ patientflow/viz/prob_dist_plot.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 def prob_dist_plot(prob_dist_data, title_, directory_path = None, figsize = (6,3), include_titles = False, truncate_at_beds=20, text_size = None, bar_colour='#5B9BD5', file_name = None):
             
@@ -9,6 +10,9 @@ def prob_dist_plot(prob_dist_data, title_, directory_path = None, figsize = (6,3
     plt.bar(prob_dist_data.index[0:truncate_at_beds+1], 
             prob_dist_data['agg_proba'].values[0:truncate_at_beds+1],
             color=bar_colour)
+    
+    plt.xlim(-0.5, truncate_at_beds + 0.5)
+    plt.xticks(np.arange(0, truncate_at_beds + 1, 5))  # Set x-axis ticks at every 5 units
     
     if text_size:
 
