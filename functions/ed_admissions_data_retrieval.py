9392542bc4c1ac3e5af15db2468eba23cab4645b
Zella King <9799569+zmek@users.noreply.github.com>
2024-07-19 12:13:54 +0000

    uclh config for spec and yta models

--- functions/ed_admissions_data_retrieval.py
+++ functions/ed_admissions_data_retrieval.py
@@ -8,16 +8,12 @@ from pathlib import Path
 
 import pandas as pd
 
-
 def safe_literal_eval(s):
     try:
-        # Check if the string is "nan" or empty and return None
-        if pd.isna(s) or str(s).strip().lower() == "nan":
+        if pd.isna(s) or str(s).strip().lower() in ["nan", "none", ""]:
             return None
-        # Otherwise, evaluate it as a tuple
         return ast.literal_eval(s)
     except (ValueError, SyntaxError):
-        # Return None if there's any other error during evaluation
         return None
 
 
@@ -30,29 +26,14 @@ def ed_admissions_get_data(path_ed_data):
 
     """
     path = os.path.join(Path().home(), path_ed_data)
-
-    # read dataframe
     df = pd.read_csv(path, parse_dates=True)
 
-    # sort by visit and date if in dataset
-    sort_columns = [
-        col for col in ["visit_number", "snapshot_datetime"] if col in df.columns
-    ]
+    sort_columns = [col for col in ["visit_number", "snapshot_datetime"] if col in df.columns]
     if sort_columns:
         df.sort_values(sort_columns, inplace=True)
 
-    # convert strings to tuples
-    if "prediction_time" in df.columns:
-        df["prediction_time"] = df["prediction_time"].apply(
-            lambda x: ast.literal_eval(x)
-        )
-
-    if "consultation_sequence" in df.columns:
-        df["consultation_sequence"] = df["consultation_sequence"].apply(
-            safe_literal_eval
-        )
-
-    if "final_sequence" in df.columns:
-        df["final_sequence"] = df["final_sequence"].apply(safe_literal_eval)
+    for column in ["prediction_time", "consultation_sequence", "final_sequence"]:
+        if column in df.columns:
+            df[column] = df[column].apply(safe_literal_eval)
 
     return df
