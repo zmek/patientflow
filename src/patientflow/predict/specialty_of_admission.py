b7fe2a3e6dbf5f9ca07fa278902f999a0a2fda43
zmek <zella.king@gmail.com>
2024-09-16 10:11:39 +0100

    test script relative imports

--- src/patientflow/predict/specialty_of_admission.py
+++ src/patientflow/predict/specialty_of_admission.py
@@ -73,7 +73,9 @@ def get_specialty_probs(
     """
 
     # Convert consultation_sequence to tuple if not already a tuple
-    if not isinstance(snapshots_df["consultation_sequence"].iloc[0], tuple):
+    if len(snapshots_df["consultation_sequence"]) > 0 and not isinstance(
+        snapshots_df["consultation_sequence"].iloc[0], tuple
+    ):
         snapshots_df.loc[:, "consultation_sequence"] = snapshots_df[
             "consultation_sequence"
         ].apply(lambda x: tuple(x) if x else ())
