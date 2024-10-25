e15476998248f608bc028ac4d08d1294e44cd897
zmek <zella.king@ucl.ac.uk>
2024-10-25 10:45:26 +0000

    add aurpc to discharge training script

--- src/patientflow/training/discharges.py
+++ src/patientflow/training/discharges.py
@@ -5,7 +5,7 @@ from sklearn.compose import ColumnTransformer
 
 from patientflow.load import get_model_name 
 from patientflow.prepare import get_snapshots_at_prediction_time
-from patientflow.train import chronological_cross_validation, initialise_xgb #create_column_transformer, 
+from patientflow.train import initialise_xgb, calculate_class_balance #create_column_transformer, chronological_cross_validation
 from sklearn.model_selection import ParameterGrid, cross_validate
 from sklearn.pipeline import Pipeline
 
@@ -15,9 +15,85 @@ from sklearn.metrics import (
     ConfusionMatrixDisplay,
     log_loss,
     roc_auc_score,
+    average_precision_score
 )
 from joblib import dump, load
 import json
+
+
+
+
+def chronological_cross_validation(pipeline, X, y, n_splits=5):
+    """
+    Perform time series cross-validation.
+
+    :param pipeline: The machine learning pipeline (preprocessing + model).
+    :param X: Input features.
+    :param y: Target variable.
+    :param n_splits: Number of splits for cross-validation.
+    :return: Dictionary with the average training and validation scores.
+    """
+    # Initialize TimeSeriesSplit
+    tscv = TimeSeriesSplit(n_splits=n_splits)
+
+    # Lists to collect scores for each fold
+    train_aucs = []
+    train_loglosses = []
+    train_auprcs = []
+
+    valid_aucs = []
+    valid_loglosses = []
+    valid_auprcs = []
+
+    # Iterate over train-test splits
+    for train_index, test_index in tscv.split(X):
+        X_train, X_valid = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_valid = y.iloc[train_index], y.iloc[test_index]
+
+        # Fit the pipeline to the training data
+        # Note that you don't need to manually transform the data; the pipeline handles it
+        pipeline.fit(X_train, y_train)
+
+        # # To access transformed feature names:
+        # transformed_cols = pipeline.named_steps['feature_transformer'].get_feature_names_out()
+        # transformed_cols = [col.split('__')[-1] for col in transformed_cols]
+
+        # Evaluate on the training split
+        y_train_pred = pipeline.predict_proba(X_train)[:, 1]
+        train_auc = roc_auc_score(y_train, y_train_pred)
+        train_logloss = log_loss(y_train, y_train_pred)
+        train_auprc = average_precision_score(y_train, y_train_pred)
+        train_aucs.append(train_auc)
+        train_loglosses.append(train_logloss)
+        train_auprcs.append(train_auprc)
+        
+        # Evaluate on the validation split
+        y_valid_pred = pipeline.predict_proba(X_valid)[:, 1]
+        valid_auc = roc_auc_score(y_valid, y_valid_pred)
+        valid_logloss = log_loss(y_valid, y_valid_pred)
+        valid_auprc = average_precision_score(y_valid, y_valid_pred)
+        valid_aucs.append(valid_auc)
+        valid_loglosses.append(valid_logloss)
+        valid_auprcs.append(valid_auprc)
+
+    # Calculate mean scores
+    mean_train_auc = sum(train_aucs) / n_splits
+    mean_train_logloss = sum(train_loglosses) / n_splits
+    mean_train_auprc = sum(train_auprcs) / n_splits
+    mean_valid_auc = sum(valid_aucs) / n_splits
+    mean_valid_logloss = sum(valid_loglosses) / n_splits
+    mean_valid_auprc = sum(valid_auprcs) / n_splits
+    
+    return {
+        "train_auc": mean_train_auc,
+        "valid_auc": mean_valid_auc,
+        "train_logloss": mean_train_logloss,
+        "valid_logloss": mean_valid_logloss,
+        "train_auprc": mean_train_auprc,
+        "valid_auprc": mean_valid_auprc
+    }
+
+
 
 
 def create_column_transformer(df, ordinal_mappings=None, top_n_subspecialties=20):
@@ -115,12 +191,24 @@ def train_discharges_models(
         X_test, y_test = get_snapshots_at_prediction_time(test_visits, _prediction_time, _exclude_from_training_data, 
                                           single_snapshot_per_visit=True, label_col = label_col)
 
+        y_train_class_balance = calculate_class_balance(y_train)
+        y_valid_class_balance = calculate_class_balance(y_valid)
+        y_test_class_balance = calculate_class_balance(y_test)
+
         # save size of each set
-        model_metadata[_model_name]['train_valid_test_set_no'] = {
-            'train_set_no' : len(X_train),
-            'valid_set_no' : len(X_valid),
-            'test_set_no' : len(X_test),
+        model_metadata[_model_name]["train_valid_test_set_no"] = {
+            "train_set_no": len(X_train),
+            "valid_set_no": len(X_valid),
+            "test_set_no": len(X_test),
         }
+
+        # save class balance of each set
+        model_metadata[_model_name]["train_valid_test_class_balance"] = {
+            "y_train_class_balance": y_train_class_balance,
+            "y_valid_class_balance": y_valid_class_balance,
+            "y_test_class_balance": y_test_class_balance,
+        }
+
 
         # iterate through the grid of hyperparameters
         for g in ParameterGrid(grid):
@@ -144,6 +232,8 @@ def train_discharges_models(
                 'valid_auc': cv_results['valid_auc'],
                 'train_logloss': cv_results['train_logloss'],
                 'valid_logloss': cv_results['valid_logloss'],
+                'train_auprc': cv_results['train_auprc'],  # New metric
+                'valid_auprc': cv_results['valid_auprc']   # New metric
             }
 
             # Update and save best model if current model is better on validation set
@@ -163,10 +253,11 @@ def train_discharges_models(
                 y_test_pred_proba = pipeline.predict_proba(X_test)[:, 1]
                 test_auc = roc_auc_score(y_test, y_test_pred_proba)
                 test_logloss = log_loss(y_test,y_test_pred_proba)
-
+                test_auprc = average_precision_score(y_test, y_test_pred_proba)
                 model_metadata[_model_name]['test_set_results'] = {
                     'test_auc' : test_auc,
-                    'test_logloss' : test_logloss
+                    'test_logloss' : test_logloss,
+                    'test_auprc' : test_auprc
                 }
 
                 # save the best features
@@ -187,3 +278,5 @@ def train_discharges_models(
 
     with open(full_path_results_dict, "w") as f:
         json.dump(model_metadata, f) 
+        
+    return model_metadata
