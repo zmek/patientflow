6ce45bc8d50f58ef7943bcc46e7c6de05a445dd5
zmek <zella.king@gmail.com>
2025-01-28 15:12:20 +0000

    make model_name variable

--- src/patientflow/viz/shap_plot.py
+++ src/patientflow/viz/shap_plot.py
@@ -46,9 +46,9 @@ def plot_shap_individually(
         ax.set_xlabel("SHAP Value")
         plt.tight_layout()
 
-        MODEL__ED_ADMISSIONS__NAME = get_model_name(model_name, _prediction_time)
+        model_name = get_model_name(model_name, _prediction_time)
         shap_plot_path = (
-            str(media_file_path / "shap_plot_") + MODEL__ED_ADMISSIONS__NAME + ".png"
+            str(media_file_path / "shap_plot_") + model_name + ".png"
         )
 
         plt.savefig(shap_plot_path)
